455f7c1e5f51e5d7c51ffffc601532b11ed166bb
Lyzygy21 <Lyzygy21@users.noreply.github.com>
2023-12-25 10:25:22 -0700

    Enhanced chat functionality

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
-def main():
-    print("Hello, World!")
+def chat():
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'quit':
+            break
+        response = process_input(user_input)
+        print(f"Escapa: {response}")
+
+def process_input(user_input):
+    # Simple response logic, can be expanded later
+    if "hello" in user_input.lower():
+        return "Hello there!"
+    else:
+        return "working on expanding capabilities"
 
 if __name__ == "__main__":
-    main()
+    print("The question is the shape of your key, will you escape?")
+    print("-----------------Welcome to Escapa---------------------")
+    chat()
